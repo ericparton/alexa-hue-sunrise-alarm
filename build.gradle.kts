8166a5c651d7f42014d516ae07266d58cde8453f
Eric Parton <ericparton@gmail.com>
2019-02-10 16:03:22 -0500

    add TODO to submit pull request for hue library

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.8.5")
+    //TODO: submit pull request for changes to this library and try to get it up on jcenter or maven central
     implementation("com.github.zeroone3010:yetanotherhueapi:0.2.0-SNAPSHOT")
 }
 
@@ -22,6 +23,5 @@ application {
 docker {
     javaApplication {
         baseImage.set("openjdk:11-jdk-stretch")
-
     }
 }
